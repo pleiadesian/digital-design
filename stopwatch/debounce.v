f246d145d930914822fdf5f939a2cd7930b01ab5
pleiadesian <wzl574402791@outlook.com>
2019-11-02 13:57:35 +0800

    finish stopwatch lab

--- stopwatch/debounce.v
+++ stopwatch/debounce.v
@@ -5,35 +5,49 @@ module debounce(clk, key_in, key_out);
 	reg		key_out;
 	
 	// counter during debounce procedure
-	reg cnt;
+	reg [31:0] cnt;
 	reg key_valid;
+	reg key_invalid;
+	reg key_temp;
 	
 	// debounce for 500000 * 20ns = 10ms
 	localparam DEBOUNCE_TIME = 500_000;
 	
 	initial begin
 		key_valid <= 1;
+		key_invalid <= 0;
 		cnt <= 0;
+		key_temp <= 1;
 	end
 	
 	// output key value after debounce finished
 	always @ (posedge clk)
 		begin
-			if (~key_valid) begin
-				cnt <= cnt + 1;
+			if (key_invalid) begin
+				key_valid <= 0;
 			end
+			if (key_valid) 
+				begin
+					key_out <= key_in;
+					key_temp <= key_in;
+				end
+			else
+				begin
+					cnt <= cnt + 1;
+					key_out <= key_temp;
+					if (cnt > 500000) begin
+						key_valid <= 1;
+						cnt <= 0;
+					end
+				end
 		end
 	always @ (negedge key_in)
 		begin
 			if (key_valid) begin
-				key_valid <= 0;
+				key_invalid <= 1;
 			end
-			if (~key_valid) begin
-				if (cnt > 500000) begin
-					key_valid <= 1;
-					key_out <= key_in;
-					cnt <= 0;
-				end
+			else begin
+				key_invalid <= 0;
 			end
 		end
 endmodule
